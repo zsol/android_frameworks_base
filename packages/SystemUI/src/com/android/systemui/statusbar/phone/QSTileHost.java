990518dc8e7d744479c0bc2668a104aff7967962
BigBrother1984 <stevewatersy@gmail.com>
2014-04-10 18:07:26 +0200

    Base: Floating Window
    Floating window contributors:
    @drcmda
    @beerbong
    @D4rKn3sSyS
    @Squadzone
    
    @LorDClockaN: Chrome floating fix
    
    VRToxin work:
    
    NL-BlackDragon:* Force immersive mode during float mode
                   * Kill floating activities running in the same thread when tapping outside.
                   * Fix showing maximize button when it can't be used
                   * Fix immersive mode snap points
                   * Fix minimize/maximize button behavior
                   * Fix minimize/maximize buttons coloring
                   * When closing floating window also kill task
                   * Add app title to Floating's title bar
                   * Added some tinted code by XplodWild.
                   * Add floating window AppCircleSideBar support.
                   * Stock recents float mode
                   * Hide notification float button when disabled
                   * only show maximize button when in normal
                     floating window view and only show the minimize
                     button when in full screen view
    
    Signed-off-by: NL-BlackDragon <blackdragon.vrtoxin@gmail.com>
    
    rogersb11:     * For fucks sake, convert new google multiwindow to previous float mode keeping some improvements
                   * Add old policies and preload needed classes for PhoneWindow
                   * Change the way PWM handles window calls/functions
                   * Full Marshmallow adaption
                   * Add floating intent to extra features AppSideBar, Slim recents
                   * Allow using new google MULTI window button in stock recents and give option for single float mode as well
                   * Slim actions float mode
                   * Floating and slim floats tiles and buttons and slim action
                   * Fix spacing of buttons in recents header
                   * Fix nonstop fatal exception on creating windows
    
    Screw'd:
    Modified for Screw'd AOSP
    QS Tiles modified for our QSTuner implementation
    
    AICPfy:
    Make it work with AICP source
    Actions, tile, overrides, etc etc...
    
    Change-Id: I81ba0bfaf8e6f29388e07beb78197dd316a11de7
    Signed-off-by: rogersb11 <brettrogers11@gmail.com>
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>
    
    Modified for use on temasek

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -53,6 +53,7 @@ import com.android.systemui.qs.tiles.DndTile;
 import com.android.systemui.qs.tiles.EditTile;
 import com.android.systemui.qs.tiles.ExpandedDesktopTile;
 import com.android.systemui.qs.tiles.FlashlightTile;
+import com.android.systemui.qs.tiles.FloatingWindowsTile;
 import com.android.systemui.qs.tiles.HeadsUpTile;
 import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.IntentTile;
@@ -325,7 +326,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
     public SecurityController getSecurityController() {
         return mSecurity;
     }
-    
+
     @Override
     public void onTuningChanged(String key, String newValue) {
         if (!CMSettings.Secure.QS_TILES.equals(key)) {
@@ -415,6 +416,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (tileSpec.equals("power_menu")) return new PowerMenuTile(this);
         else if (tileSpec.equals("caffeine")) return new CaffeineTile(this);
         else if (tileSpec.equals("sound")) return new SoundTile(this);
+        else if (tileSpec.equals("float_mode")) return new FloatingWindowsTile(this);
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
         else throw new IllegalArgumentException("Bad tile spec: " + tileSpec);
     }
@@ -521,6 +523,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (spec.equals("power_menu")) return R.string.quick_settings_power_menu_label;
         else if (spec.equals("caffeine")) return R.string.quick_settings_caffeine_label;
         else if (spec.equals("sound")) return R.string.quick_settings_sound_label;
+        else if (spec.equals("float_mode")) return R.string.recent_float_mode_title;
         return 0;
     }
 
@@ -565,6 +568,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (spec.equals("power_menu")) return R.drawable.ic_qs_power_menu;
         else if (spec.equals("caffeine")) return R.drawable.ic_qs_caffeine_on;
         else if (spec.equals("sound")) return R.drawable.ic_qs_ringer_audible;
+        else if (spec.equals("float_mode")) return R.drawable.ic_qs_floating_on;
         return 0;
     }
 
