9adba765839f1e3288767aea92603e9d1d8f9363
Evisceration <eviscerationls@gmail.com>
2014-06-18 13:54:39 +0200

    aapt: we dont care about uncommented public symbols
    export SHOW_UNCOMMENTED_SYMBOL_WARNING to show them
    
    Change-Id: I6e6461b54e5cd3a9c09efeb8df3172e48bbfa8e5

--- tools/aapt/Resource.cpp
+++ tools/aapt/Resource.cpp
@@ -2515,9 +2515,11 @@ static status_t writeSymbolClass(
                     "%s/** %s\n",
                     getIndentSpace(indent), cmt.string());
         } else if (sym.isPublic && !includePrivate) {
+#ifdef SHOW_UNCOMMENTED_SYMBOL_WARNING
             sym.sourcePos.warning("No comment for public symbol %s:%s/%s",
                 assets->getPackage().string(), className.string(),
                 String8(sym.name).string());
+#endif
         }
         String16 typeComment(sym.typeComment);
         if (typeComment.size() > 0) {
@@ -2561,9 +2563,11 @@ static status_t writeSymbolClass(
                     getIndentSpace(indent), cmt.string(),
                     getIndentSpace(indent));
         } else if (sym.isPublic && !includePrivate) {
+#ifdef SHOW_UNCOMMENTED_SYMBOL_WARNING
             sym.sourcePos.warning("No comment for public symbol %s:%s/%s",
                 assets->getPackage().string(), className.string(),
                 String8(sym.name).string());
+#endif
         }
         ann.printAnnotations(fp, getIndentSpace(indent));
         fprintf(fp, "%spublic static final String %s=\"%s\";\n",
