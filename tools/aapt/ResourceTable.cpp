9f0dff4c6a3e283c588a4be5e8b2e57bf60ada4c
Dave Kessler <activethrasher00@gmail.com>
2014-11-25 19:19:09 -0500

    AAPT: STFU-BBQ
    Silence obnx string warnings and make it easier to scroll up to build errors
    
    Change-Id: I1cf9306e6714ab1e0b8189408c65f05348e89337

--- tools/aapt/ResourceTable.cpp
+++ tools/aapt/ResourceTable.cpp
@@ -2691,8 +2691,8 @@ ResourceTable::validateLocalizations(void)
 #ifdef SHOW_DEFAULT_TRANSLATION_WARNINGS
         // Look for strings with no default localization
         if (configSrcMap.count(defaultLocale) == 0) {
-            SourcePos().warning("string '%s' has no default translation.",
-                    String8(nameIter->first).string());
+            NOISY(SourcePos().warning("string '%s' has no default translation.",
+                    String8(nameIter->first).string()));
             if (mBundle->getVerbose()) {
                 for (map<String8, SourcePos>::const_iterator locales = configSrcMap.begin();
                     locales != configSrcMap.end();
@@ -2749,10 +2749,10 @@ ResourceTable::validateLocalizations(void)
                      iter++) {
                     configStr.appendFormat(" %s", iter->string());
                 }
-                SourcePos().warning("string '%s' is missing %u required localizations:%s",
+                NOISY(SourcePos().warning("string '%s' is missing %u required localizations:%s",
                         String8(nameIter->first).string(),
                         (unsigned int)missingConfigs.size(),
-                        configStr.string());
+                        configStr.string()));
             }
         }
 #endif
