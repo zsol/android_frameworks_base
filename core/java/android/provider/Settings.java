6678df5967e1122ad37c3f0e3c363d68acda1090
cristianomatos <cristianobmatos@gmail.com>
2014-12-31 10:33:44 -0200

    AOKP system animations: remove no app override
    - Removed until we find a better solution regarding recents animations

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3747,8 +3747,7 @@ public final class Settings {
                 "task_open_behind",
         };  
         public static final String ANIMATION_CONTROLS_DURATION = "animation_controls_duration";
-        public static final String ANIMATION_CONTROLS_NO_OVERRIDE = "animation_controls_no_override";
-
+        
         /**
          * Whether to display app circle sidebar
          * @hide
