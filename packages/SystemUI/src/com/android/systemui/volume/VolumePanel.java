bcecef0fd391905e12d98769f4c5cca0bf7d4c44
david <davidteri91@gmail.com>
2015-01-23 08:55:05 +0100

    SystemUI: Update 'Materialize volume panel in/out animation'
    Fix animation expanded volume panel hide
    Add animation for volume panel expand
    
    This update the following commit: 5e46b21
    
    Conflicts:
    
    	packages/SystemUI/src/com/android/systemui/volume/VolumePanel.java

--- packages/SystemUI/src/com/android/systemui/volume/VolumePanel.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumePanel.java
@@ -18,6 +18,7 @@ package com.android.systemui.volume;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
+import android.animation.LayoutTransition;
 import android.animation.ValueAnimator;
 import android.app.AlertDialog;
 import android.app.Dialog;
@@ -496,6 +497,11 @@ public class VolumePanel extends Handler implements DemoMode {
             window.findViewById(com.android.systemui.R.id.zen_buttons_container)
                 .setBackground(null);
         }
+
+        LayoutTransition lt = new LayoutTransition();
+        lt.disableTransitionType(LayoutTransition.CHANGE_DISAPPEARING);
+        lt.disableTransitionType(LayoutTransition.DISAPPEARING);
+        mSliderPanel.setLayoutTransition(lt);
     }
 
     public VolumePanel(Context context, ZenModeController zenController) {
@@ -1623,12 +1629,12 @@ public class VolumePanel extends Handler implements DemoMode {
 
             case MSG_TIMEOUT: {
                 if (isShowing()) {
-                    hideVolumePanel();
                     if (mDialog != null) {
                         mView.animate().y(-mView.getHeight())
                                 .setDuration(ANIMATION_DURATION)
                                 .withEndAction(new Runnable() {
                             public void run() {
+                                hideVolumePanel();
                                 mDialog.dismiss();
                                 clearRemoteStreamController();
                                 mActiveStreamType = -1;
