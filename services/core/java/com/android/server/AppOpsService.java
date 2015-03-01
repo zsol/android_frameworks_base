77143885827f52cbf3e25fecd18cb14092af0662
Natiq Ahmed <mnatiq@codeaurora.org>
2015-03-01 16:52:14 +0530

    Compilation fixes
    Change-Id: I22ff2d238023660761cfe3cfc5367b72e4afecf8

--- services/core/java/com/android/server/AppOpsService.java
+++ services/core/java/com/android/server/AppOpsService.java
@@ -1008,6 +1008,14 @@ public class AppOpsService extends IAppOpsService.Stub {
         }
     }
 
+    private void scheduleWriteNowLocked() {
+        if (!mWriteScheduled) {
+            mWriteScheduled = true;
+        }
+        mHandler.removeCallbacks(mWriteRunner);
+        mHandler.post(mWriteRunner);
+    }
+
     private Op getOpLocked(int code, int uid, String packageName, boolean edit) {
         Ops ops = getOpsLocked(uid, packageName, edit);
         if (ops == null) {
@@ -1715,7 +1723,7 @@ public class AppOpsService extends IAppOpsService.Stub {
                 }
             }
             // ensure the counter reset persists
-            scheduleFastWriteLocked();
+            scheduleWriteNowLocked();
         }
     }
 }
