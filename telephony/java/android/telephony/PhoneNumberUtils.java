fab2bf1c193157fe63468c2df5afef2c35bdce08
Qiang Chen <cqiang@codeaurora.org>
2015-03-12 15:33:06 +0800

    Disable the plus sign conversion in non-NANP system
    Support NANP international conversion and other telephone numbering plans.
    Return the original dial string if the phone is used in non-NANP system.
    
    CRs-Fixed: 791454
    
    Change-Id: I10d2822d0406021e6f631e722e812ad714074995

--- telephony/java/android/telephony/PhoneNumberUtils.java
+++ telephony/java/android/telephony/PhoneNumberUtils.java
@@ -2288,8 +2288,8 @@ public class PhoneNumberUtils
                 if (useNanp) {
                     networkDialStr = extractNetworkPortion(tempDialStr);
                 } else  {
-                    networkDialStr = extractNetworkPortionAlt(tempDialStr);
-
+                    Rlog.e("checkAndProcessPlusCode:non-NANP not supported", dialStr);
+                    return dialStr;
                 }
 
                 networkDialStr = processPlusCode(networkDialStr, useNanp);
