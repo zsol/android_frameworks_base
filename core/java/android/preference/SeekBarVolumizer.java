5c2f20cbd3d583292e8390e8d8972297d4ea2348
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-03-13 05:20:46 +0000

    Base: fix unlinking volume sliders (2/2)
    Change-Id: I3591cc459b8532e1a6a1d7acbc394b076ff58ccb

--- core/java/android/preference/SeekBarVolumizer.java
+++ core/java/android/preference/SeekBarVolumizer.java
@@ -103,8 +103,15 @@ public class SeekBarVolumizer implements OnSeekBarChangeListener, Handler.Callba
         mDefaultUri = defaultUri;
     }
 
-    private static boolean isNotificationOrRing(int stream) {
-        return stream == AudioManager.STREAM_RING || stream == AudioManager.STREAM_NOTIFICATION;
+    private boolean isNotificationOrRing(int stream) {
+        boolean linkNotifications = Settings.Secure.getInt(mContext.getContentResolver(),
+                        Settings.Secure.VOLUME_LINK_NOTIFICATION, 1) == 1;
+        if (!linkNotifications) {
+            return stream == AudioManager.STREAM_RING;
+        } else {
+            return stream == AudioManager.STREAM_RING
+                || stream == AudioManager.STREAM_NOTIFICATION;
+        }
     }
 
     public void setSeekBar(SeekBar seekBar) {
