0b482a617a4b274c6f69f0a9a2cc801557398514
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-03-30 11:47:18 +0100

    SystemUI: update notification volume slider state when toggling modes
    Change-Id: Ic05bdf16a61fdca3ab09f6d4b938facf315718a0

--- packages/SystemUI/src/com/android/systemui/volume/VolumePanel.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumePanel.java
@@ -743,6 +743,9 @@ public class VolumePanel extends Handler implements DemoMode {
                     public void onClick(View v) {
                         resetVolPanelTimeout();
                         toggleRinger(sc);
+                        if (!mVolumeLinkNotification) {
+                            updateStates();
+                        }
                     }
                 });
                 sc.iconSuppressedRes = com.android.systemui.R.drawable.ic_ringer_mute;
