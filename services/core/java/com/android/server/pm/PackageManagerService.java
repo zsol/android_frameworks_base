VanirAOSP/frameworks_base

Show package names during dexopt
ps2: Attempt to determine localized package label, and print that.
     If unavailable, print packageName

Change-Id: If9ca78fe312acd674bdcee160f914b4445d5c85f

services/java/com/android/server/pm/PackageManagerService.java
@@ -4040,8 +4040,18 @@ public class PackageManagerService extends IPackageManager.Stub {
             final int pkgsSize = pkgs.size();
             ExecutorService executorService = Executors.newFixedThreadPool(sNThreads);
             final long start = System.currentTimeMillis();
+            PackageManager pm = null;;
+            if (mContext != null)
+                pm = mContext.getPackageManager();
+            String n = null;
             for (PackageParser.Package pkg : pkgs) {
                 final PackageParser.Package p = pkg;
+                if (pm != null)
+                    n = (String)p.applicationInfo.loadLabel(pm);
+                if (n == null || n.length() == 0)
+                    n = p.packageName;
+                final String name = "\n"+n;
+                n = null;
                 synchronized (mInstallLock) {
                     if (!p.mDidDexOpt) {
                         executorService.submit(new Runnable() {
@@ -4049,7 +4059,7 @@ public class PackageManagerService extends IPackageManager.Stub {
                             public void run() {
                                 if (!isFirstBoot()) {
                                     i[0]++;
-                                    postBootMessageUpdate(i[0], pkgsSize);
+                                    postBootMessageUpdate(i[0], pkgsSize, name);
                                 }
                                 performDexOptLI(p, false, false, true);
                             }
@@ -4057,7 +4067,7 @@ public class PackageManagerService extends IPackageManager.Stub {
                     } else {
                         if (!isFirstBoot()) {
                             i[0]++;
-                            postBootMessageUpdate(i[0], pkgsSize);
+                            postBootMessageUpdate(i[0], pkgsSize, name);
                         }
                     }
                 }
@@ -4073,12 +4083,12 @@ public class PackageManagerService extends IPackageManager.Stub {
         }
     }
 
-    private void postBootMessageUpdate(int n, int total) {
+    private void postBootMessageUpdate(int n, int total, final String name) {
         try {
             ActivityManagerNative.getDefault().showBootMessage(
                     mContext.getResources().getString(
                             com.android.internal.R.string.android_upgrading_apk,
-                            n, total), true);
+                            n, total, name), true);
         } catch (RemoteException e) {
         }
     }

zsol/android_frameworks_base

services/core/java/com/android/server/pm/PackageManagerService.java
@@ -4883,13 +4883,25 @@ public class PackageManagerService extends IPackageManager.Stub {
             if (lowThreshold == 0) {
                 throw new IllegalStateException("Invalid low memory threshold");
             }
+
+            PackageManager pm = null;;
+            if (mContext != null)
+                pm = mContext.getPackageManager();
+            String n = null;
+
             for (PackageParser.Package pkg : sortedPkgs) {
                 long usableSpace = dataDir.getUsableSpace();
                 if (usableSpace < lowThreshold) {
                     Log.w(TAG, "Not running dexopt on remaining apps due to low memory: " + usableSpace);
                     break;
                 }
-                performBootDexOpt(pkg, ++i, total);
+                if (pm != null)
+                    n = (String)pkg.applicationInfo.loadLabel(pm);
+                if (n == null || n.length() == 0)
+                    n = pkg.packageName;
+                final String name = "\n"+n;
+                n = null;
+                performBootDexOpt(pkg, ++i, total, name);
             }
         }
     }
@@ -4938,6 +4950,10 @@ public class PackageManagerService extends IPackageManager.Stub {
     }
 
     private void performBootDexOpt(PackageParser.Package pkg, int curr, int total) {
+        performBootDexOpt(pkg, curr, total, "");
+    }
+
+    private void performBootDexOpt(PackageParser.Package pkg, int curr, int total, String pkgname) {
         if (DEBUG_DEXOPT) {
             Log.i(TAG, "Optimizing app " + curr + " of " + total + ": " + pkg.packageName);
         }
@@ -4945,7 +4961,7 @@ public class PackageManagerService extends IPackageManager.Stub {
             try {
                 ActivityManagerNative.getDefault().showBootMessage(
                         mContext.getResources().getString(R.string.android_upgrading_apk,
-                                curr, total), true);
+                                curr, total, pkgname), true);
             } catch (RemoteException e) {
             }
         }