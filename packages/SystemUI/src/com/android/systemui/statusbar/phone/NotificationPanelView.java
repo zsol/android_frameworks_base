1ecf0daa2750676e6f67c1cb39a89714ef7a970f
Roman Birg <roman@cyngn.com>
2015-04-06 15:44:44 -0700

    SystemUI: move keyguard visualizer behind notifications
    Also, handle hiding and showing the visualizer when dragging
    notifications, or tapping on them.
    
    Change-Id: I9402dded1cd76676887caae5f3aa075b03a6e7ea
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Conflicts:
    
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -1084,7 +1084,7 @@ public class NotificationPanelView extends PanelView implements
     private void setKeyguardBottomAreaVisibility(int statusBarState,
             boolean goingToFullShade) {
         if (goingToFullShade) {
-            mKeyguardBottomArea.requestVisualizer(false, 0);
+            mStatusBar.requestVisualizer(false, 0);
             mKeyguardBottomArea.animate().cancel();
             mKeyguardBottomArea.animate()
                     .alpha(0f)
@@ -1123,7 +1123,7 @@ public class NotificationPanelView extends PanelView implements
                         .setDuration(mStatusBar.getKeyguardFadingAwayDuration()/2)
                         .start();
             }
-            mKeyguardBottomArea.requestVisualizer(false, 0);
+            mStatusBar.requestVisualizer(false, 0);
         } else if (mStatusBarState == StatusBarState.SHADE_LOCKED
                 && statusBarState == StatusBarState.KEYGUARD) {
             mKeyguardStatusView.animate().cancel();
@@ -1136,7 +1136,7 @@ public class NotificationPanelView extends PanelView implements
                     .setDuration(320)
                     .setInterpolator(PhoneStatusBar.ALPHA_IN)
                     .withEndAction(mAnimateKeyguardStatusViewVisibleEndRunnable);
-            mKeyguardBottomArea.requestVisualizer(true, 320);
+            mStatusBar.requestVisualizer(true, 320);
         } else if (statusBarState == StatusBarState.KEYGUARD) {
             mKeyguardStatusView.animate().cancel();
             mKeyguardStatusViewAnimating = false;
@@ -1872,7 +1872,7 @@ public class NotificationPanelView extends PanelView implements
     @Override
     public void onSwipingAnimationFinished(boolean snappingBack) {
         if (snappingBack) {
-            mKeyguardBottomArea.requestVisualizer(true, 300);
+            mStatusBar.requestVisualizer(true, 300);
         }
     }
 
@@ -1992,7 +1992,7 @@ public class NotificationPanelView extends PanelView implements
 
     public void setEmptyDragAmount(float amount) {
         if (amount == 0 && mStatusBarState == StatusBarState.KEYGUARD) {
-            mKeyguardBottomArea.requestVisualizer(true, 300);
+            mStatusBar.requestVisualizer(true, 300);
         }
         float factor = 0.8f;
         if (mNotificationStackScroller.getNotGoneChildCount() > 0) {
