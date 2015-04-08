f9e67dc2dcdac533556263872ee599d0765bccd4
Squadzone <ai.the.smarties.physics@gmail.com>
2015-04-08 13:32:32 -0700

    Implement ambient display as Active Display
    Currently Ambient Display fully support if device has
      Significant motion sensor and wake gesture sensor
    
    with this change allow Ambient Display to work like
        Active Display
    
    -- Allow wake up using shake event
    -- use different class to manage sensor
    
    enable this via overlay custom_config.xml
    
    inside Frameworks base
    
        <!-- Doze: force using accelerometer as pick up sensor -->
        <bool name="config_dozeUseAccelerometer">false</bool>
    
    inside System UI
    
        <!-- Doze: shake accelerometer threshold -->
        <integer name="doze_shake_accelerometer_threshold">10</integer>
    
    also dont forget to setup other value for Ambient Display
    
    see https://gerrit.omnirom.org/10140
    
    PS. 12 : prepare for user configurations
    
    <!------------------------------------------------------------>
    
    NOTE:
       Shake event aka Wake up will not triggered until
       Proximity is Covered (need to put inside pocket)
    
       for shake accelerometer, you must setup based on your device
       each device has different threshold
    
    <!------------------------------------------------------------>
    
    NOTE 2 :
    
       you can also overwrite all value for Ambient Display inside BUILD.PROP
       but i recommend only overwrite value from overlay fw base and system Ui
    
       example value inside BUILD.PROP :
    
       <!-- Doze: does this device support STATE_DOZE and STATE_DOZE_SUSPEND?  -->
       doze.display.supported=true
    
       <!-- Doze: pulse parameter - how long does it take to fade in? -->
       doze.pulse.duration.in=1000
    
       <!-- Doze: pulse parameter - once faded in, how long does it stay visible? -->
       doze.pulse.duration.visible=3000
    
       <!-- Doze: pulse parameter - how long does it take to fade out? -->
       doze.pulse.duration.out=1000
    
       <!-- Doze: should notifications be used as a pulse signal? -->
       doze.pulse.notifications=true
    
       <!-- Doze: when to pulse after a buzzworthy notification arrives -->
       doze.pulse.schedule=1s,10s,30s,60s,120s
    
       <!-- Doze: maximum number of times the notification pulse schedule can be reset -->
       doze.pulse.schedule.resets=3
    
       <!-- Doze: shake accelerometer threshold -->
       doze.shake.acc.threshold=10
    
       <!-- Doze: force using accelerometer as pick up sensor -->
       doze.use.accelerometer=true
    
    <!------------------------------------------------------------>
    
    With this change now we have 3 mode :
    
        1. pocket mode = proximity enabled after screen off, if detect NEAR state (covered by hand or somethings), doze will disable
              until FAR state (uncovered), if FAR state is triggered, Doze will appears.
    
        2. pulse mode = doze is triggered by notifications, and so after notifications arrive, doze will show up, and doze will setup
              time to show again (see doze.pulse.schedule=1s,10s,30s,60s,120s inside config.xml) and reset after see doze.pulse.schedule.resets=3
        pulse mode by default is already there if device is support ambient display (e.g nexus 6), so there is no change in the code
        (still using original code logic)
    
        3. shake mode = enabled after proximity detect FAR state (pocket mode) or Doze is triggerd by notifications (pulse mode), and disabled
              if proximity detect NEAR state, or user already shake
    
    <!------------------------------------------------------------>
    
    Feature describes:
        1. Full mode (pocket,shake,pulse)
        2. Pocket and shake only
        3. Pocket and pulse only
        4. Pocket only
        5. Shake and pulse only
        6. Pulse only (default)
    
    <!------------------------------------------------------------>
    
    Note :
        Shake mode only enabled after Doze is triggered by proximity or notifications (depends on option)
        so if doze not triggered by that, shake mode will stay disabled.
    
    <!------------------------------------------------------------>
    
    AICPfy
    
    Change-Id: I6e1d634c9c8b1c3d3ac938c572d9f79902e6e9e5

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4085,6 +4085,60 @@ public final class Settings {
         public static final String HEADS_UP_NOTIFCATION_DECAY = "heads_up_notification_decay";
 
         /**
+         * Enable/disable overwrite default value for ambient display
+         * @hide
+         */
+        public static final String DOZE_OVERWRITE_VALUE = "doze_overwrite_value";
+
+        /**
+         * Duration in for ambient display
+         * @hide
+         */
+        public static final String DOZE_PULSE_DURATION_IN = "doze_pulse_duration_in";
+
+        /**
+         * Duration visible for ambient display
+         * @hide
+         */
+        public static final String DOZE_PULSE_DURATION_VISIBLE = "doze_pulse_duration_visible";
+
+        /**
+         * Duration out for ambient display
+         * @hide
+         */
+        public static final String DOZE_PULSE_DURATION_OUT = "doze_pulse_duration_out";
+
+        /**
+         * Pulse if there is notifications for ambient display
+         * @hide
+         */
+        public static final String DOZE_PULSE_ON_NOTIFICATIONS = "doze_pulse_on_notifications";
+
+        /**
+         * Shake threshold for ambient display
+         * @hide
+         */
+        public static final String DOZE_SHAKE_ACC_THRESHOLD = "doze_shake_acc_threshold";
+
+        /**
+         * Pocket mode for ambient display
+         * @hide
+         */
+        public static final String DOZE_POCKET_MODE = "doze_pocket_mode";
+
+        /**
+         * Shake mode for ambient display
+         * @hide
+         */
+        public static final String DOZE_SHAKE_MODE = "doze_shake_mode";
+
+        /**
+         * Time mode for ambient display
+         * @hide
+         */
+        public static final String DOZE_TIME_MODE = "doze_time_mode";
+
+        /**
          * Show when WiFi or data mobile is sending/receiving data
          * @hide
          */
