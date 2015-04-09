4b6db365f5e4b4b84fdb31d2b29f7e6d7bbd0041
Jorge Ruesga <jorge@ruesga.com>
2015-04-09 23:31:34 +0100

    systemui: animate clear recents button on exit animation
    Change-Id: Ic617f50400558be4ad92a4a5ea3ef441d2afde6a
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- packages/SystemUI/src/com/android/systemui/recents/RecentsActivity.java
+++ packages/SystemUI/src/com/android/systemui/recents/RecentsActivity.java
@@ -583,6 +583,9 @@ public class RecentsActivity extends Activity implements RecentsView.RecentsView
 
         // Dismiss Recents to the focused Task or Home
         dismissRecentsToFocusedTaskOrHome(true);
+
+        // Hide clear recents button before return to home
+        mRecentsView.startHideClearRecentsButtonAnimation();
     }
 
     /** Called when debug mode is triggered */
@@ -628,6 +631,9 @@ public class RecentsActivity extends Activity implements RecentsView.RecentsView
     public void onTaskLaunchFailed() {
         // Return to Home
         dismissRecentsToHomeRaw(true);
+
+        // Hide clear recents button before return to home
+        mRecentsView.startHideClearRecentsButtonAnimation();
     }
 
     @Override
