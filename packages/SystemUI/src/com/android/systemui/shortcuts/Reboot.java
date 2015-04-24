AICP/frameworks_base

Frameworks: Slim Shortcuts
Change-Id: I784b4fb37238658131779eae3b55adc0aefd8d18

packages/SystemUI/src/com/android/systemui/shortcuts/Reboot.java
@@ -1,0 +1,49 @@
+/*
+ * Copyright 2013 SlimRom
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.shortcuts;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.PowerManager;
+
+import com.android.internal.util.slim.Action;
+
+public class Reboot extends Activity  {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Handler handle = new Handler();
+        // Allow statusbar to collapse if desired
+        handle.postDelayed(new Runnable() {
+            public void run() {
+                PowerManager pm =
+                        (PowerManager) Reboot.this.getSystemService(
+                        Context.POWER_SERVICE);
+                pm.reboot("");
+                Reboot.this.finish();
+            }
+        }, 500);
+    }
+}

zsol/android_frameworks_base

Conflicts:
	packages/SystemUI/AndroidManifest.xml

packages/SystemUI/src/com/android/systemui/shortcuts/Reboot.java
@@ -1,0 +1,49 @@
+/*
+ * Copyright 2013 SlimRom
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.shortcuts;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.PowerManager;
+
+import com.android.internal.util.temasek.Action;
+
+public class Reboot extends Activity  {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Handler handle = new Handler();
+        // Allow statusbar to collapse if desired
+        handle.postDelayed(new Runnable() {
+            public void run() {
+                PowerManager pm =
+                        (PowerManager) Reboot.this.getSystemService(
+                        Context.POWER_SERVICE);
+                pm.reboot("");
+                Reboot.this.finish();
+            }
+        }, 500);
+    }
+}