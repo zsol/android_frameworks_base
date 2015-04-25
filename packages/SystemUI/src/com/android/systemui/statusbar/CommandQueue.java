AICP/frameworks_base

Action: Add screenshot action
Change-Id: I98960f6bc9987da763342f491611dc54c44fca43

packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
@@ -63,6 +63,7 @@ public class CommandQueue extends IStatusBar.Stub {
     private static final int MSG_HIDE_HEADS_UP                      = 21 << MSG_SHIFT;
     private static final int MSG_TOGGLE_LAST_APP                    = 22 << MSG_SHIFT;
     private static final int MSG_TOGGLE_KILL_APP                    = 23 << MSG_SHIFT;
+    private static final int MSG_TOGGLE_SCREENSHOT                  = 24 << MSG_SHIFT;
 
     public static final int FLAG_EXCLUDE_NONE = 0;
     public static final int FLAG_EXCLUDE_SEARCH_PANEL = 1 << 0;
@@ -111,6 +112,7 @@ public class CommandQueue extends IStatusBar.Stub {
         public void scheduleHeadsUpClose();
         public void toggleLastApp();
         public void toggleKillApp();
+        public void toggleScreenshot();
     }
 
     public CommandQueue(Callbacks callbacks, StatusBarIconList list) {
@@ -291,8 +293,8 @@ public class CommandQueue extends IStatusBar.Stub {
     public void resume() {
         mPaused = false;
     }
-	
-	public void toggleLastApp() {
+
+    public void toggleLastApp() {
         synchronized (mList) {
             mHandler.removeMessages(MSG_TOGGLE_LAST_APP);
             mHandler.obtainMessage(MSG_TOGGLE_LAST_APP, 0, 0, null).sendToTarget();
@@ -303,6 +305,13 @@ public class CommandQueue extends IStatusBar.Stub {
         synchronized (mList) {
             mHandler.removeMessages(MSG_TOGGLE_KILL_APP);
             mHandler.obtainMessage(MSG_TOGGLE_KILL_APP, 0, 0, null).sendToTarget();
+        }
+    }
+
+    public void toggleScreenshot() {
+        synchronized (mList) {
+            mHandler.removeMessages(MSG_TOGGLE_SCREENSHOT);
+            mHandler.obtainMessage(MSG_TOGGLE_SCREENSHOT, 0, 0, null).sendToTarget();
         }
     }
 
@@ -401,12 +410,15 @@ public class CommandQueue extends IStatusBar.Stub {
                 case MSG_HIDE_HEADS_UP:
                     mCallbacks.scheduleHeadsUpClose();
                     break;
-               case MSG_TOGGLE_LAST_APP:
+                case MSG_TOGGLE_LAST_APP:
                     mCallbacks.toggleLastApp();
                     break;
                 case MSG_TOGGLE_KILL_APP:
                     mCallbacks.toggleKillApp();
-                    break;	
+                    break;
+                case MSG_TOGGLE_SCREENSHOT:
+                    mCallbacks.toggleScreenshot();
+                    break;
             }
         }
     }

zsol/android_frameworks_base

Conflicts:
	packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java

packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
@@ -62,8 +62,9 @@ public class CommandQueue extends IStatusBar.Stub {
     private static final int MSG_START_CUSTOM_INTENT_AFTER_KEYGUARD = 20 << MSG_SHIFT;
     private static final int MSG_HIDE_HEADS_UP                      = 21 << MSG_SHIFT;
     private static final int MSG_SET_PIE_TRIGGER_MASK               = 22 << MSG_SHIFT;
-    private static final int MSG_TOGGLE_LAST_APP                    = 22 << MSG_SHIFT;
-    private static final int MSG_TOGGLE_KILL_APP                    = 23 << MSG_SHIFT;
+    private static final int MSG_TOGGLE_LAST_APP                    = 23 << MSG_SHIFT;
+    private static final int MSG_TOGGLE_KILL_APP                    = 24 << MSG_SHIFT;
+    private static final int MSG_TOGGLE_SCREENSHOT                  = 25 << MSG_SHIFT;
 
     public static final int FLAG_EXCLUDE_NONE = 0;
     public static final int FLAG_EXCLUDE_SEARCH_PANEL = 1 << 0;
@@ -113,6 +114,7 @@ public class CommandQueue extends IStatusBar.Stub {
         public void setPieTriggerMask(int newMask, boolean lock);
         public void toggleLastApp();
         public void toggleKillApp();
+        public void toggleScreenshot();
     }
 
     public CommandQueue(Callbacks callbacks, StatusBarIconList list) {
@@ -314,6 +316,13 @@ public class CommandQueue extends IStatusBar.Stub {
         }
     }
 
+    public void toggleScreenshot() {
+        synchronized (mList) {
+            mHandler.removeMessages(MSG_TOGGLE_SCREENSHOT);
+            mHandler.obtainMessage(MSG_TOGGLE_SCREENSHOT, 0, 0, null).sendToTarget();
+        }
+    }
+
     private final class H extends Handler {
         public void handleMessage(Message msg) {
             if (mPaused) {
@@ -417,7 +426,10 @@ public class CommandQueue extends IStatusBar.Stub {
                     break;
                 case MSG_TOGGLE_KILL_APP:
                     mCallbacks.toggleKillApp();
-                    break;	
+                    break;
+                case MSG_TOGGLE_SCREENSHOT:
+                    mCallbacks.toggleScreenshot();
+                    break;
             }
         }
     }