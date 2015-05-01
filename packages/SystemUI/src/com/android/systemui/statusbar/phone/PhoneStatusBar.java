0d9cd4b6de4ec1d5120668485088a91888eb006e
Shaun Eaton <shaunsibu@gmail.com>
2015-05-01 00:31:28 -0600

    Add missing line to update NavBar upon theme change.
    Change-Id: I31bdbda8eb98c89c2dec0e2daa5434bf8eb46b5b

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1042,6 +1042,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         if (mNavigationBarView == null) {
             mNavigationBarView =
                 (NavigationBarView) View.inflate(context, R.layout.navigation_bar, null);
+            mNavigationBarView.updateResources(getNavbarThemedResources());
         }
 
         if (mRecreating) {
