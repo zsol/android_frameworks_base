8996a482cda066f01c81bcc0394aa56461e6e5f3
Janson Kang <temasek71@gmail.com>
2015-05-03 09:29:42 +0800

    Revert "Fix fetching application context for ThemedUiContext."
    This reverts commit d8d394ab8eb1bde3d011d974d3f024549b1ed6e8.

--- core/java/android/content/pm/ThemeUtils.java
+++ core/java/android/content/pm/ThemeUtils.java
@@ -512,7 +512,7 @@ public class ThemeUtils {
         try {
             Context uiContext = context.createPackageContext("com.android.systemui",
                     Context.CONTEXT_RESTRICTED);
-            return new ThemedUiContext(uiContext, context.getApplicationContext());
+            return new ThemedUiContext(uiContext, context.getPackageName());
         } catch (PackageManager.NameNotFoundException e) {
         }
 
@@ -574,21 +574,16 @@ public class ThemeUtils {
     }
 
     private static class ThemedUiContext extends ContextWrapper {
-        private Context mAppContext;
+        private String mPackageName;
 
-        public ThemedUiContext(Context context, Context appContext) {
+        public ThemedUiContext(Context context, String packageName) {
             super(context);
-            mAppContext = appContext;
-        }
-
-        @Override
-        public Context getApplicationContext() {
-            return mAppContext;
+            mPackageName = packageName;
         }
 
         @Override
         public String getPackageName() {
-            return mAppContext.getPackageName();
+            return mPackageName;
         }
     }
 
