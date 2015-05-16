61d51133138648cf93ba1734a428243d7aef5a7c
rmcc <ricardo@cyngn.com>
2015-05-16 10:21:55 -0400

    Theme reboot menu
    This was used way back in original T-Mobile theme engine.
    Unsure of the original author but used this commit:
    So I gave him authorship
    
    Squashed commit of the theme engine support.
    Author: rmcc
    https://github.com/CyanogenMod/android_frameworks_base/commit/cbdc805a9b4715d3d6f47e2b4de9e7de49569a4e
    
    Conflicts:
    	services/core/java/com/android/server/wm/WindowManagerService.java

--- core/java/android/view/WindowManagerPolicy.java
+++ core/java/android/view/WindowManagerPolicy.java
@@ -415,6 +415,8 @@ public interface WindowManagerPolicy {
 
         public void shutdown(boolean confirm);
         public void rebootSafeMode(boolean confirm);
+	public void reboot();
+ 
 
         public void rebootTile();
 
--- policy/src/com/android/internal/policy/impl/GlobalActions.java
+++ policy/src/com/android/internal/policy/impl/GlobalActions.java
@@ -539,14 +539,7 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
 
         @Override
         public void onPress() {
-            try {
-                IPowerManager pm = IPowerManager.Stub.asInterface(ServiceManager
-                        .getService(Context.POWER_SERVICE));
-                pm.reboot(true, null, false);
-            } catch (RemoteException e) {
-                Log.e(TAG, "PowerManager service died!", e);
-                return;
-            }
+            mWindowManagerFuncs.reboot();
         }
     }
 
