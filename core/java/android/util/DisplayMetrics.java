a1105d95d9acb7d4348391c479e5baa68a895c46
Roman Birg <roman@cyngn.com>
2015-05-21 06:22:24 -0700

    unbreak density scaling
    - Only apply density scaling to built in screens so we don't affect
    overlays.
    
    - Only apply density scaling for activities which fall under the
      'default' compatibility bucket. Some apps may  have different
      compatibility info setup (e.g. scaling) and we do not
      need to apply the user density to this activity.
    
    Change-Id: I0b4849141cf081694aae5668ae60764e70be7db1
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- core/java/android/util/DisplayMetrics.java
+++ core/java/android/util/DisplayMetrics.java
@@ -260,7 +260,6 @@ public class DisplayMetrics {
         noncompatScaledDensity = o.noncompatScaledDensity;
         noncompatXdpi = o.noncompatXdpi;
         noncompatYdpi = o.noncompatYdpi;
-        updateDensity();
     }
     
     public void setToDefaults() {
