a6c2dc99576b9415bda108973385f331d1cc8df4
rogersb11 <brettrogers11@gmail.com>
2015-05-28 14:07:13 -0400

    Power menu header button improvement
    Move button inside of the signal cluster so it transistions out on pulldown
    like the rest of the icons do. Much much better now, final fix
    
    Change-Id: Ia22314ac6202b2e2489fc3e9e3954c682008e036
    
    Remove icon colors which this rom doesn't support.
    
    Conflicts:
    	packages/SystemUI/res/layout/status_bar_expanded_header.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
@@ -463,21 +463,27 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
 
     private void updateSystemIconsLayoutParams() {
         RelativeLayout.LayoutParams lp = (LayoutParams) mSystemIconsSuperContainer.getLayoutParams();
-        int baseId = mTaskManagerButton != null
+        int taskManager = mTaskManagerButton != null
                 ? mTaskManagerButton.getId() : mSettingsButton.getId();
         int headsUp = mHeadsUpButton != null
                 ? mHeadsUpButton.getId()
                 : mMultiUserSwitch.getId();
+        int powerMenu = mStatusBarPowerMenu != null
+                ? mStatusBarPowerMenu.getId() : mTaskManagerButton.getId();
         int rule = mExpanded
-                ? baseId
+                ? taskManager
                 : mMultiUserSwitch.getId();
         int ruleh = mExpanded
                 ? headsUp
+                : mMultiUserSwitch.getId();
+        int rulep = mExpanded
+                ? powerMenu
                 : mMultiUserSwitch.getId();
         if (rule != lp.getRules()[RelativeLayout.START_OF] &&
                 rule != lp.getRules()[RelativeLayout.START_OF]) {
             lp.addRule(RelativeLayout.START_OF, rule);
             lp.addRule(RelativeLayout.START_OF, ruleh);
+            lp.addRule(RelativeLayout.START_OF, rulep);
             mSystemIconsSuperContainer.setLayoutParams(lp);
         }
     }
@@ -827,7 +833,6 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
         target.avatarX = mMultiUserSwitch.getLeft() + mMultiUserAvatar.getLeft();
         target.avatarY = mMultiUserSwitch.getTop() + mMultiUserAvatar.getTop();
         target.weatherY = mClock.getBottom() - mWeatherLine1.getHeight();
-        target.statusBarPowerMenuY = mClock.getTop();
         if (getLayoutDirection() == LAYOUT_DIRECTION_LTR) {
             target.batteryX = mSystemIconsSuperContainer.getLeft()
                     + mSystemIconsContainer.getRight();
