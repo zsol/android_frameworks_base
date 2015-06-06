26596e73ed9dfddf21e225c06138d53cb34ba5c5
Alexander Martinz <eviscerationls@gmail.com>
2015-06-06 18:02:09 +0200

    SystemUI: Keyguard: do not limit double tap to sleep to statusbar
    Change-Id: Ie1555f6ac7f97d5c9e257a4eb5a0bce3420a3bf4
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -746,9 +746,7 @@ public class NotificationPanelView extends PanelView implements
         if (mBlockTouches) {
             return false;
         }
-        if (mDoubleTapToSleepEnabled
-                && mStatusBarState == StatusBarState.KEYGUARD
-                && event.getY() < mStatusBarHeaderHeight) {
+        if (mDoubleTapToSleepEnabled && mStatusBarState == StatusBarState.KEYGUARD) {
             mDoubleTapGesture.onTouchEvent(event);
         }
         initDownStates(event);
