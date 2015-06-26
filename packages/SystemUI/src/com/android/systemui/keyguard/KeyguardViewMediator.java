aa1d68225ac40c2de575b9a1721ac39f00e14376
Andy Mast <andy@cyngn.com>
2015-06-26 15:19:08 -0700

    Keyguard: Authenticate with fingerprint
    Change-Id: Id547339fc1b6e06b3a9d8f6906258872986df3f1

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
@@ -471,11 +471,8 @@ public class KeyguardViewMediator extends SystemUI {
         }
 
         public void onFingerprintRecognized(int userId) {
-            if (mStatusBarKeyguardViewManager.isBouncerShowing()) {
-                mViewMediatorCallback.keyguardDone(true);
-            }
+            mViewMediatorCallback.keyguardDone(true);
         };
-
     };
 
     ViewMediatorCallback mViewMediatorCallback = new ViewMediatorCallback() {
@@ -1333,6 +1330,7 @@ public class KeyguardViewMediator extends SystemUI {
             mUpdateMonitor.clearFailedUnlockAttempts();
         }
         mUpdateMonitor.clearFingerprintRecognized();
+        mUpdateMonitor.stopAuthenticatingFingerprint();
 
         if (mExitSecureCallback != null) {
             try {
