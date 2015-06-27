4b04fa66fb9be0df3078aadc703a10a68703f769
lion0738 <lion0738@naver.com>
2015-06-27 16:38:52 -0300

    [1/2] Base: option to launch heads up in floating mode.
    Port By BlackDragon.
    Used reference: scotthartbti/android_frameworks_base@06dd21f
    
    Added Modified NotificationHelper for LP.
    
    Signed-off-by: BlackDragon <blackdragon.fusionteam@gmail.com>
    
    Change-Id: I312501371e9066ea7bed12a8027c73a4396b90cb
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/policy/HeadsUpNotificationView.java

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -118,6 +118,7 @@ import com.android.systemui.cm.SpamMessageProvider;
 import com.android.systemui.statusbar.NotificationData.Entry;
 import com.android.systemui.statusbar.NotificationData.Entry;
 import com.android.systemui.statusbar.appcirclesidebar.AppCircleSidebar;
+import com.android.systemui.statusbar.notification.NotificationHelper;
 import com.android.systemui.statusbar.phone.NavigationBarOverlay;
 import com.android.systemui.statusbar.phone.NavigationBarView;
 import com.android.systemui.statusbar.phone.StatusBarKeyguardViewManager;
@@ -336,6 +337,9 @@ public abstract class BaseStatusBar extends SystemUI implements
     public Handler getHandler() {
         return mHandler != null ? mHandler : createHandler();
     }
+
+    // Notification helper
+    protected NotificationHelper mNotificationHelper;
 
     protected final ContentObserver mSettingsObserver = new ContentObserver(mHandler) {
         @Override
@@ -688,6 +692,8 @@ public abstract class BaseStatusBar extends SystemUI implements
         // Connect in to the status bar manager service
         StatusBarIconList iconList = new StatusBarIconList();
         mCommandQueue = new CommandQueue(this, iconList);
+
+        mNotificationHelper = new NotificationHelper(this, mContext);
 
         int[] switches = new int[8];
         ArrayList<IBinder> binders = new ArrayList<IBinder>();
@@ -1676,8 +1682,8 @@ public abstract class BaseStatusBar extends SystemUI implements
 
         PendingIntent contentIntent = sbn.getNotification().contentIntent;
         if (contentIntent != null) {
-            final View.OnClickListener listener = makeClicker(contentIntent, sbn.getKey(),
-                    isHeadsUp);
+            final View.OnClickListener listener = mNotificationHelper.getNotificationClickListener
+                    (entry, isHeadsUp);
             row.setOnClickListener(listener);
         } else {
             row.setOnClickListener(null);
@@ -1846,15 +1852,20 @@ public abstract class BaseStatusBar extends SystemUI implements
         return new NotificationClicker(intent, notificationKey, forHun);
     }
 
-    protected class NotificationClicker implements View.OnClickListener {
+    public class NotificationClicker implements View.OnClickListener {
         private PendingIntent mIntent;
         private final String mNotificationKey;
         private boolean mIsHeadsUp;
+        public boolean mFloat;
 
         public NotificationClicker(PendingIntent intent, String notificationKey, boolean forHun) {
             mIntent = intent;
             mNotificationKey = notificationKey;
             mIsHeadsUp = forHun;
+        }
+
+        public void makeFloating(boolean floating) {
+            mFloat = floating;
         }
 
         public void onClick(final View v) {
@@ -1892,8 +1903,12 @@ public abstract class BaseStatusBar extends SystemUI implements
                             }
 
                             if (mIntent != null) {
+                                Intent overlay = new Intent();
+                                int flags = Intent.FLAG_FLOATING_WINDOW | Intent.FLAG_ACTIVITY_CLEAR_TASK;
+
+                            if (mFloat) overlay.addFlags(flags);
                                 try {
-                                    mIntent.send();
+                                    mIntent.send(mContext, 0, overlay);
                                 } catch (PendingIntent.CanceledException e) {
                                     // the stack trace isn't very helpful here.
                                     // Just log the exception message.
@@ -2390,8 +2405,8 @@ public abstract class BaseStatusBar extends SystemUI implements
         // update the contentIntent
         final PendingIntent contentIntent = notification.getNotification().contentIntent;
         if (contentIntent != null) {
-            final View.OnClickListener listener = makeClicker(contentIntent, notification.getKey(),
-                    isHeadsUp);
+            final View.OnClickListener listener =
+                    mNotificationHelper.getNotificationClickListener(entry, isHeadsUp);
             entry.row.setOnClickListener(listener);
         } else {
             entry.row.setOnClickListener(null);
--- packages/SystemUI/src/com/android/systemui/statusbar/policy/HeadsUpNotificationView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/HeadsUpNotificationView.java
@@ -46,6 +46,7 @@ import com.android.systemui.R;
 import com.android.systemui.SwipeHelper;
 import com.android.systemui.statusbar.ExpandableView;
 import com.android.systemui.statusbar.NotificationData;
+import com.android.systemui.statusbar.notification.NotificationHelper;
 import com.android.systemui.statusbar.phone.PhoneStatusBar;
 
 import java.util.ArrayList;
@@ -82,6 +83,9 @@ public class HeadsUpNotificationView extends FrameLayout implements SwipeHelper.
     private boolean mTouchOutside;
 
     private static int sRoundedRectCornerRadius = 0;
+
+    // Notification helper
+    protected NotificationHelper mNotificationHelper;
 
     public HeadsUpNotificationView(Context context, AttributeSet attrs) {
         this(context, attrs, 0);
@@ -141,17 +145,22 @@ public class HeadsUpNotificationView extends FrameLayout implements SwipeHelper.
         mBar = bar;
     }
 
+    public void setNotificationHelper(NotificationHelper notificationHelper) {
+		mNotificationHelper = notificationHelper;
+    }
+
     public ViewGroup getHolder() {
         return mContentHolder;
     }
 
-    public boolean showNotification(NotificationData.Entry headsUp) {
-        if (mHeadsUp != null && headsUp != null && !mHeadsUp.key.equals(headsUp.key)) {
+    public boolean showNotification(NotificationData.Entry isHeadsUp) {
+        if (mHeadsUp != null && isHeadsUp != null
+            && !mHeadsUp.key.equals(isHeadsUp.key)) {
             // bump any previous heads up back to the shade
             release();
         }
 
-        mHeadsUp = headsUp; // set new entry
+        mHeadsUp = isHeadsUp; // set new entry
 
         if (mBar.isExpandedVisible() || mBar.isImeShowing()) {
             releaseAndClose();
@@ -174,7 +183,8 @@ public class HeadsUpNotificationView extends FrameLayout implements SwipeHelper.
             mHeadsUp.row.setSensitive(false);
             mHeadsUp.row.setHeadsUp(true);
             mHeadsUp.row.setHideSensitive(
-                    false, false /* animated */, 0 /* delay */, 0 /* duration */);
+                false, false /* animated */, 0 /* delay */, 0 /* duration */);
+            mHeadsUp.expanded.setOnClickListener(mNotificationHelper.getNotificationClickListener(isHeadsUp, true));
             if (mContentHolder == null) {
                 // too soon!
                 return false;
