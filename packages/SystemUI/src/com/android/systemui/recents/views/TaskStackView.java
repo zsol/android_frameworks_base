4072c410800b2af520164daadc9775a90c59063b
Alex Cruz <mazdarider23@gmail.com>
2015-06-30 09:35:42 +0000

    Screen pinning improvements
    Inspired by the way ASUS implemented their screen pinning, I took it upon myself to do the
    same with our ROM and move the pinning icon next to the icon for dismissing a recent app.
    
    - Matched the behavior of the recent dismiss icon
    - Changes icon to dark if recent app uses a light primary color
    
    See images to see the differences
    
    AOSP/DU - http://i.imgur.com/DYoKmNL.png
    ASUS Zenfone 2 (inspiration) - http://i.imgur.com/BybyGHp.jpg
    
    What it looks like with this commit - http://i.imgur.com/TFkgn5C.png
    
    - Replaced ASUS icon with a icon covered under the creative commons license
    https://thenounproject.com/term/pin/56183/
    
    Credit/Thanks to @xWASABI for creating the vector because God knows that was kicking my ass :-)
    https://plus.google.com/u/0/101729058057775926785
    
    Change-Id: Ie3e160647ba90b99fd0e130abc79fb63ed0ad3f5
    
    Conflicts:
    	packages/SystemUI/res/layout/recents_task_view.xml

--- packages/SystemUI/src/com/android/systemui/recents/views/TaskStackView.java
+++ packages/SystemUI/src/com/android/systemui/recents/views/TaskStackView.java
@@ -959,7 +959,6 @@ public class TaskStackView extends FrameLayout implements TaskStack.TaskStackCal
             TaskView frontTv = getChildViewForTask(newFrontMostTask);
             if (frontTv != null) {
                 frontTv.onTaskBound(newFrontMostTask);
-                frontTv.fadeInActionButton(0, mConfig.taskViewEnterFromAppDuration);
             }
         }
 
