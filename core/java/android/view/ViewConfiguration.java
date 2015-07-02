d94ea23d47cae5e8264df7b5c76ffb88d26fc970
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-07-02 23:21:17 +0100

    Bring scrolling back to a sane level
    Change-Id: I3f6b37df3c430284a59b33a8dd8a04904e2dd760

--- core/java/android/view/ViewConfiguration.java
+++ core/java/android/view/ViewConfiguration.java
@@ -180,7 +180,7 @@ public class ViewConfiguration {
     /**
      * Maximum velocity to initiate a fling, as measured in dips per second
      */
-    private static final int MAXIMUM_FLING_VELOCITY = 16000;
+    private static final int MAXIMUM_FLING_VELOCITY = 12000;
 
     /**
      * Delay before dispatching a recurring accessibility event in milliseconds.
@@ -200,7 +200,7 @@ public class ViewConfiguration {
     /**
      * The coefficient of friction applied to flings/scrolls.
      */
-    private static final float SCROLL_FRICTION = 0.007f;
+    private static final float SCROLL_FRICTION = 0.010f;
 
     /**
      * Max distance in dips to overscroll for edge effects
