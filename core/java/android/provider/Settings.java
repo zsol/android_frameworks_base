9794a6b7d1ac4891f87d87e0d0fc7a87d03c6f50
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2015-07-02 18:51:32 +0200

    [2/2] SlimHeadsUp: Add a global 3-way switch
    disabled : will disable all headsup
    perapp: notifications are shown as headsup per app (current behaviour)
    forced : will force all notifications to be shown as headsup
    
    Forced mode will :
    
    - not break global snooze
    - not override non-intrusive incoming call setting
    - not mess up current per app user settings
    
    Change-Id: I1dc1b4c84d601149f21eca0722fc1f586d33ba3e
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2322,6 +2322,13 @@ public final class Settings {
         public static final String QS_USE_MAIN_TILES = Secure.QS_USE_MAIN_TILES;
 
         /**
+         * Defines global heads up enable/disable switch.
+         *
+         * @hide
+         */
+        public static final String HEADS_UP_GLOBAL_SWITCH = "heads_up_global_switch";
+
+        /**
          * Defines global heads up snooze time if user want to snooze it.
          *
          * @hide
