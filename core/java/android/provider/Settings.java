d71834c880e8cd04d629d19c7ab29fe53707e65c
XXMrHyde <xxmrhyde@gmx.co.uk>
2015-07-05 02:48:38 +0200

    Lock screen weather: Add options to hide the weather when notifications are visible, (1/2):
    Hide the weather when:
      - Visible notifications reach the maximum allowed notifications on the lock screen
        (including the notification overlay container)
      - Visible notifications reach a customizable number of notifications
      - Never
    
    Customizable number of notifications (to hide the weather panel):
      - 1 (always)
      - 2
      - 3
      - 4
      - 5
      - 6
    
    Change-Id: I5fc6bc76d1f781bbfe3c463df3d5be3e602fa94d

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2243,6 +2243,37 @@ public final class Settings {
                 "lockscreen_shortcuts_longpress";
 
         /**
+         * Whether to hide the weather panel when the notifications will reach the number of notifications
+         * @hide
+         */
+        public static final String LOCK_SCREEN_WEATHER_HIDE_PANEL = "lock_screen_weather_hide_panel";
+
+        /**
+         * Hide the weather panel when the visible lockscreen notifications will reach this number
+         * 1: 1 notification (always)
+         * 2: 2 notifications
+         * 3: 3 notifications
+         * 4: 4 notifications
+         * 5: 5 notifications
+         * 6: 6 notifications
+         * default: 6
+         * @hide
+         */
+        public static final String LOCK_SCREEN_WEATHER_NUMBER_OF_NOTIFICATIONS = "lock_screen_weather_number_of_notifications";
+
+        /**
+         * Number of notifications visible on the lockscreen including the overflow container
+         * @hide
+         */
+        public static final String LOCK_SCREEN_VISIBLE_NOTIFICATIONS = "lock_screen_visible_notifications";
+
+        /**
+         * Number of maximum allowed notifications on the lock screen
+         * @hide
+         */
+        public static final String LOCK_SCREEN_MAX_NOTIFICATIONS = "lock_screen_max_notifications";
+
+        /**
          * Whether to allow one finger quick settings expansion on the right side of the statusbar.
          * @deprecated Use {@link android.provider.Settings.System#QS_QUICK_PULLDOWN} instead
          * @hide
