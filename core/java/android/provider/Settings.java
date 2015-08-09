437cd077c12292b9824194626f0eebf8860adfbf
BlackDragon <blackdragon.fusionteam@gmail.com>
2015-08-09 13:50:55 +0200

    TRDS: Set Notifications and QS colors on theme change (1/2)
    PS1: Add override custom colors switch.
    PS2: Also to change colors on auto mode.
    
    Signed-off-by: BlackDragon <blackdragon.fusionteam@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4867,6 +4867,12 @@ public final class Settings {
         public static final String SHOW_HEADSET_ICON = "show_headset_icon";
 
         /**
+         * TRDS: Override custom notification and qs colors on theme change
+         * @hide
+         */
+        public static final String OVERRIDE_CUSTOM_COLORS = "override_custom_colors";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
