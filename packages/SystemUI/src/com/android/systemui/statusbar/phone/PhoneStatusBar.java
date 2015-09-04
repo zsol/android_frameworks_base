0b5daf9794588e191af53fb35446a6d146773c5b
Roman Birg <roman@cyngn.com>
2015-09-04 15:55:39 -0700

    SystemUI: hide visualizer when keyguard is occluded
    Change-Id: Ia943fd829f7a379fee0f657bb08c34b8c6f36caa
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarKeyguardViewManager.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -5795,8 +5795,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         }
     }
 
-    protected void setVisualizerAlpha(float alpha) {
-        mVisualizerView.setAlpha(alpha);
+    public VisualizerView getVisualizer() {
+        return mVisualizerView;
     }
 
     private final class ShadeUpdates {
