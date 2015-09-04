27502ca4e40d9d17ed5c18bfbf6829d51d57e081
rogersb11 <brettrogers11@gmail.com>
2015-09-04 04:26:39 -0400

    FWB:Status bar carrier label font size 2/2

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4268,6 +4268,12 @@ public final class Settings {
          */
         public static final String STATUS_BAR_CARRIER_COLOR = "status_bar_carrier_color";
 
+         /**
+         * Settings for carrier label font size
+         * @hide
+         */
+        public static final String STATUS_BAR_CARRIER_FONT_SIZE = "status_bar_carrier_font_size";        
+
         /**
          * Width of the gesture anywhere trigger.
          * @hide
