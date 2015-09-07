bc2a21fc02ad62a94ad5683cfc73f46d3bffddcc
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-09-07 01:28:37 +0100

    StatusBarHeader: update headsup button onclick
    Change-Id: I1cc97f9c90f37d74c813b0f6c3cd845c9ed46025

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
@@ -659,6 +659,7 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
             startForecastActivity();
         } else if (v == mHeadsUpButton) {
             startHeadsUpActivity();
+            updateHeadsUpButton();
         }
         mQSPanel.vibrateTile(20);
     }
