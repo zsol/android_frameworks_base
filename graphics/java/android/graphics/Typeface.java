8f27897db1fd36855453033d9813f46c124e236d
Janson Kang <temasek71@gmail.com>
2015-09-10 16:21:01 +0800

    Revert "Themes: Fix bootloop when applying theme fonts"
    This reverts commit 57cc663499bab29c9f010846051d5795293e3f64.

--- graphics/java/android/graphics/Typeface.java
+++ graphics/java/android/graphics/Typeface.java
@@ -416,14 +416,12 @@ public class Typeface {
             }
             for (FontListParser.Alias alias : fontConfig.aliases) {
                 Typeface base = systemFonts.get(alias.toName);
-                if (base != null) {
-                    Typeface newFace = base;
-                    int weight = alias.weight;
-                    if (weight != 400) {
-                        newFace = new Typeface(nativeCreateWeightAlias(base.native_instance, weight));
-                    }
-                    systemFonts.put(alias.name, newFace);
+                Typeface newFace = base;
+                int weight = alias.weight;
+                if (weight != 400) {
+                    newFace = new Typeface(nativeCreateWeightAlias(base.native_instance, weight));
                 }
+                systemFonts.put(alias.name, newFace);
             }
             sSystemFontMap = systemFonts;
 
@@ -446,7 +444,7 @@ public class Typeface {
      */
     public static void recreateDefaults() {
         sTypefaceCache.clear();
-        if (sSystemFontMap != null) sSystemFontMap.clear();
+        sSystemFontMap.clear();
         init();
 
         DEFAULT_INTERNAL = create((String) null, 0);
