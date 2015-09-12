9d68c9016134fbb44eefdf8c84b620e3ab8ff047
beanstown106 <nbenis106@gmail.com>
2015-09-12 22:18:00 +0800

    Disable Lockscreen Media Art [1/2]
    * toggle to choose whether or not to show album art/media art on lockscreen
    
    Special thanks to @atl4tis from Cataclysm rom for pointing out where to hook into this
    
    Change-Id: I1250c052f61331e72b31c36974e0a5aa724935eb

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2275,6 +2275,12 @@ public final class Settings {
         public static final String LOCK_SCREEN_MAX_NOTIFICATIONS = "lock_screen_max_notifications";
 
         /**
+         * Whether to show media art on lockscreen
+         * @hide
+         */
+        public static final String LOCKSCREEN_MEDIA_METADATA = "lockscreen_media_metadata";
+
+        /**
          * Whether to allow one finger quick settings expansion on the right side of the statusbar.
          * @deprecated Use {@link android.provider.Settings.System#QS_QUICK_PULLDOWN} instead
          * @hide
