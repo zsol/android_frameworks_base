5d65983b2cfb1bd498f7a1868665cebdaeb66865
Janson Kang <temasek71@gmail.com>
2015-10-03 10:51:02 +0800

    Revert "StatusBarHeader: update headsup button onclick"
    This reverts commit bc2a21fc02ad62a94ad5683cfc73f46d3bffddcc.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
@@ -677,7 +677,6 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
             startForecastActivity();
         } else if (v == mHeadsUpButton) {
             startHeadsUpActivity();
-            updateHeadsUpButton();
         }
         mQSPanel.vibrateTile(20);
     }
