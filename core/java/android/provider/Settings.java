c743fba3b75915ee4cf6029fea7f747964ac0b3f
Janson Kang <temasek71@gmail.com>
2015-10-10 11:05:08 +0800

    Revert "SystemUI : Navbar ring five targets"
    ATM no means to make it look good for all devices.
    Some look good, some bad so revert for now.
    
    This reverts commit c11e706861d42ef1d6059768800067d6d927c9a1.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7623,8 +7623,6 @@ public final class Settings {
                 "navigation_ring_targets_0",
                 "navigation_ring_targets_1",
                 "navigation_ring_targets_2",
-                "navigation_ring_targets_3",
-                "navigation_ring_targets_4",
         };
 
         /**
