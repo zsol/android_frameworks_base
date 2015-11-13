e2374de83a45da9c9a1055f299bf2e4623ef8d56
cristianomatos <cristianobmatos@gmail.com>
2015-11-13 10:10:25 -0200

    Move KILL_APP_LONGPRESS_TIMEOUT to Settings.Secure
    Change-Id: Id0fafc0066bf2d173a51071f647721ac2132c8fb

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2414,12 +2414,6 @@ public final class Settings {
         };
 
         /**
-         * Timeout of long-pressing the Back button
-         * @hide
-         */
-        public static final String KILL_APP_LONGPRESS_TIMEOUT = "kill_app_longpress_timeout";
-
-        /**
          * Scaling factor for fonts, float.
          */
         public static final String FONT_SCALE = "font_scale";
@@ -5835,6 +5829,12 @@ public final class Settings {
          * @hide
          */
         public static final String SCREENSAVER_DEFAULT_COMPONENT = "screensaver_default_component";
+
+        /**
+         * Timeout of long-pressing the Back button
+         * @hide
+         */
+        public static final String KILL_APP_LONGPRESS_TIMEOUT = "kill_app_longpress_timeout";
 
         /**
          * The default NFC payment component
