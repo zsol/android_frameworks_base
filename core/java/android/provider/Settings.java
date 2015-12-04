c7d4e4e2d043b742fa3537964a727241ead1c74b
maxwen <max.weninger@gmail.com>
2015-12-04 19:44:47 +0100

    [1/2] base: unlink notification volume is back
    also add long press dnd tile to open details
    without changing state
    
    -delete old unused volume images
    -add new one for notification volumes
    maybe some day one will be able to create a vector
    drawable for this - I am not :)
    
    AICPfy:
    - remove DND tile change
    
    Change-Id: Id26021832ad1ca5e579878ee2471f7c401bc74ac
    Signed-off-by: Janson Kang <temasek71@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4187,6 +4187,13 @@ public final class Settings {
         public static final String ANIMATION_CONTROLS_REVERSE_EXIT = "animation_controls_reverse_exit";
 
         /**
+         * Boolean value whether to link ringtone and notification volume
+         *
+         * @hide
+         */
+        public static final String VOLUME_LINK_NOTIFICATION = "volume_link_notification";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
