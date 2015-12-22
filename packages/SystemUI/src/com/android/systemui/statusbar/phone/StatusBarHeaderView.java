faeba676ada5d6435e4d5cd5204bf15ccc78c8cc
Roman Birg <roman@cyngn.com>
2015-12-21 16:27:52 -0800

    SystemUI: add tiles detail view
    Change-Id: I5da6b6ac4045992faab8dee87e11398c7807b241
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
@@ -166,6 +166,8 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
     private SettingsObserver mSettingsObserver;
     private boolean mShowWeather;
     private boolean mShowBatteryTextExpanded;
+
+    private QSTile.DetailAdapter mEditingDetailAdapter;
     private boolean mEditing;
 
     private ImageView mBackgroundImage;
@@ -794,13 +796,8 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
 
     public void setEditing(boolean editing) {
         mEditing = editing;
-        if (mEditing) {
-            mQsPanelCallback.onShowingDetail(new QSTile.DetailAdapter() {
-                @Override
-                public StatusBarPanelCustomTile getCustomTile() {
-                    return null;
-                }
-
+        if (mEditingDetailAdapter == null) {
+            mEditingDetailAdapter = new QSTile.DetailAdapter() {
                 @Override
                 public int getTitle() {
                     return R.string.quick_settings_edit_label;
@@ -822,6 +819,11 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
                 }
 
                 @Override
+                public StatusBarPanelCustomTile getCustomTile() {
+                    return null;
+                }
+
+                @Override
                 public void setToggleState(boolean state) {
 
                 }
@@ -830,10 +832,10 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
                 public int getMetricsCategory() {
                     return MetricsConstants.DONT_TRACK_ME_BRO;
                 }
-            });
-        } else {
-            mQsPanelCallback.onShowingDetail(null);
+            };
         }
+        mQsPanelCallback.onShowingDetail(mEditing ? mEditingDetailAdapter : null);
+        updateEverything();
     }
 
     /**
@@ -917,7 +919,7 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
             post(new Runnable() {
                 @Override
                 public void run() {
-                    handleShowingDetail(detail);
+                    handleShowingDetail(mEditing && detail == null ? mEditingDetailAdapter : detail);
                 }
             });
         }
