cacc41862e18c9a2f4997901108534b733d94b3c
XXMrHyde <xxmrhyde@gmx.co.uk>
2016-01-13 09:56:19 -0700

    Notifications: Custom colors, (1/2):
    ("normal", heads up and lock screen notifications)
    
    - Media background color mode:
      - Never colorize the background
      - Colorize the background if the notification doesn`t include a own color
      - Always colorize the background
    - App icon background color mode:
      - Never colorize the background
      - Colorize the background if the notification doesn`t include a own color
      - Always colorize the background
    - App icon color mode:
      - Never colorize the background
      - Colorize the icon if the icon is an greyscale icon
      - Always colorize the background
    - Background color
    - Background color, (on long click)
    - App icon background color
    - Text color
    - Icon colors,
      (app-, action- and media action icons)
    
    Fixes and additions:
    - Use different colors for high/normal or minor priority
      (minor priority color is a darker/lighter color
      of the high/normal color)
    - Don`t use greyscale icons when dozing
    - Show the app icon background when dozing,
      (there is no color inverting, using a dark/black background
      will hide the background)
    
    Known bugs:
    
    Media notifications:
    - Some media notifications doesn`t respect the choosen action icon color
      and text colors
      (e.g. PowerAmp. Notifications from Google Play Music and Eleven
      are working without any problems)
    
    Notifications (general):
    - Some notifications doesn`t respect a new text color,
      (if the notification is already present,
      new notifications are always using the "right" text color,
      e.g. GMail, Hangouts, ... seems it`s a similar issue like the one I fixed with
      the "Notifications: Fix dark text on dark background" commit,
      but unfortunately this fix doesn`t work in this case)
    
    May be there are more bugs with notifications from other apps,
    or scenarios I didn`t tested.
    
    Change-Id: Ia625ba27cd417538869397ea7b1a662d0bfaf559

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -109,6 +109,8 @@ import com.android.internal.util.cm.SpamFilter.SpamContract.NotificationTable;
 import com.android.internal.util.cm.SpamFilter.SpamContract.PackageTable;
 import com.android.internal.util.NotificationColorUtil;
 import com.android.internal.util.omni.OmniSwitchConstants;
+import com.android.internal.util.temasek.ColorHelper;
+import com.android.internal.util.temasek.NotificationColorHelper;
 import com.android.internal.widget.LockPatternUtils;
 import com.android.keyguard.KeyguardUpdateMonitor;
 import com.android.systemui.DejankUtils;
