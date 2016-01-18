530f73d55c0b9a818cd6f5e36ca55e343866de96
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-01-19 01:01:33 +0200

    SystemUI: Fix copy-pasta of battery saver tile
    * Fixes NIGHTLIES-2354
    
    Change-Id: I214435bdb8dc29086055c3a1230cc8a5fd4c32c1

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -544,7 +544,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (spec.equals("pie")) return R.drawable.ic_qs_pie_on;	
         else if (spec.equals("restartui")) return R.drawable.ic_qs_systemui_restart;
         else if (spec.equals("heads_up")) return R.drawable.ic_qs_heads_up_on;
-        else if (spec.equals("heads_up")) return R.drawable.ic_qs_battery_saver_on;
+        else if (spec.equals("battery_saver")) return R.drawable.ic_qs_battery_saver_on;
         return 0;
     }
 
