8fad6a3dd4990edd0420a1f93da90c542df18ffe
Nico60 <ndhouailly@gmail.com>
2016-01-24 17:19:32 +0100

    Hide arrows in network traffic indicators [1/2]
    Change-Id: I846c1091d2f88f27f26bf2e9f8e2ac2c6bd67a07
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3853,6 +3853,12 @@ public final class Settings {
         public static final String NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD = "network_traffic_autohide_threshold";
 
         /**
+         * Whether to disable showing arrows in network traffic indicators
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_HIDEARROW = "network_traffic_hidearrow";
+
+        /**
          * show clear all recents button
          *  @hide
          */
