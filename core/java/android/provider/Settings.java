94c630ca1422ec0d8b3d399a7fb4ca52d794814b
rogersb11 <brettrogers11@gmail.com>
2016-02-02 22:46:10 -0500

    Heads Up Snooze  2/2
    Change-Id: I776a9b5b2f411e4940070e76ce705fc7f055b68

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4815,6 +4815,13 @@ public final class Settings {
         public static final String QS_NUM_TILE_COLUMNS = "sysui_qs_num_tile_columns";
 
         /**
+         * Snooze time for heads up
+         *
+         * @hide
+         */
+        public static final String HEADS_UP_SNOOZE_LENGTH_MS = "heads_up_snooze_length_ms";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
