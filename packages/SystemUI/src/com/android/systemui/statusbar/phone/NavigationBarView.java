937e92ea6e5266b83d18ff477c8ead135dd2409e
Janson Kang <temasek71@gmail.com>
2016-03-16 17:38:11 +0800

    Revert "SystemUI: Allow clearing recents tasks within recents button"
    This reverts commit ece65f133b2970ee3ba2fadecb1ec49a028744f4.
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/recents/RecentsActivity.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -96,7 +96,6 @@ public class NavigationBarView extends LinearLayout {
     private Drawable mRecentIcon;
     private Drawable mRecentLandIcon;
     private Drawable mHomeIcon, mHomeLandIcon;
-    private Drawable mRecentAltIcon, mRecentAltLandIcon;
 
     private boolean mDimNavButtons;
     private int mDimNavButtonsTimeout;
@@ -399,8 +398,6 @@ public class NavigationBarView extends LinearLayout {
         mRecentLandIcon = mRecentIcon;
         mHomeIcon = res.getDrawable(R.drawable.ic_sysbar_home);
         mHomeLandIcon = mHomeIcon;
-        mRecentAltIcon = res.getDrawable(R.drawable.ic_sysbar_recent_clear);
-        mRecentAltLandIcon = mRecentAltIcon;
     }
 
     public void updateResources(Resources res) {
@@ -471,10 +468,7 @@ public class NavigationBarView extends LinearLayout {
                 ? (mVertical ? mBackAltLandIcon : mBackAltIcon)
                 : (mVertical ? mBackLandIcon : mBackIcon));
 
-        ((ImageView)getRecentsButton()).setImageDrawable(
-                    (0 != (hints & StatusBarManager.NAVIGATION_HINT_RECENT_ALT))
-                            ? (mVertical ? mRecentAltLandIcon : mRecentAltIcon)
-                            : (mVertical ? mRecentLandIcon : mRecentIcon));
+        ((ImageView)getRecentsButton()).setImageDrawable(mVertical ? mRecentLandIcon : mRecentIcon);
         ((ImageView)getHomeButton()).setImageDrawable(mVertical ? mHomeLandIcon : mHomeIcon);
 
         final boolean showImeButton = ((hints & StatusBarManager.NAVIGATION_HINT_IME_SHOWN) != 0)
@@ -524,10 +518,6 @@ public class NavigationBarView extends LinearLayout {
 
     private void setSideButtonVisibility(boolean left, int vis) {
         mSideButtonVisibilities[mVertical ? 1 : 0][left ? 0 : 1] = vis;
-    }
-
-    public int getNavigationIconHints() {
-        return mNavigationIconHints;
     }
 
     public void setDisabledFlags(int disabledFlags) {
