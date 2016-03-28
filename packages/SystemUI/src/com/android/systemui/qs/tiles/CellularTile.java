AICP/frameworks_base

CellularTile: Long-click triggers detail view instead
LTE Tile long-click already triggers mobile network settings,
don't replicate it.

Change-Id: I39144ee5eab83482fd2152ca2cdfff52e1e25818

packages/SystemUI/src/com/android/systemui/qs/tiles/CellularTile.java
@@ -111,10 +111,14 @@ public class CellularTile extends QSTile<QSTile.SignalState> {
 
     @Override
     protected void handleLongClick() {
-        if (mTelephonyManager.getDefault().getPhoneCount() > 1) {
-            mHost.startSettingsActivity(MOBILE_NETWORK_SETTINGS_MSIM);
+        if (mDataController.isMobileDataSupported()) {
+            showDetail(true);
         } else {
-            mHost.startSettingsActivity(MOBILE_NETWORK_SETTINGS);
+            if (mTelephonyManager.getDefault().getPhoneCount() > 1) {
+                mHost.startSettingsActivity(MOBILE_NETWORK_SETTINGS_MSIM);
+            } else {
+                mHost.startSettingsActivity(MOBILE_NETWORK_SETTINGS);
+            }
         }
     }
 

zsol/android_frameworks_base

Signed-off-by: Janson Kang <temasek71@gmail.com>

packages/SystemUI/src/com/android/systemui/qs/tiles/CellularTile.java
@@ -124,10 +124,14 @@ public class CellularTile extends QSTile<QSTile.SignalState> {
 
     @Override
     protected void handleLongClick() {
-        if (mTelephonyManager.getDefault().getPhoneCount() > 1) {
-            mHost.startActivityDismissingKeyguard(MOBILE_NETWORK_SETTINGS_MSIM);
+        if (mDataController.isMobileDataSupported()) {
+            showDetail(true);
         } else {
-            mHost.startActivityDismissingKeyguard(MOBILE_NETWORK_SETTINGS);
+            if (mTelephonyManager.getDefault().getPhoneCount() > 1) {
+                mHost.startActivityDismissingKeyguard(MOBILE_NETWORK_SETTINGS_MSIM);
+            } else {
+                mHost.startActivityDismissingKeyguard(MOBILE_NETWORK_SETTINGS);
+            }
         }
     }
 