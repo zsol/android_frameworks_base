9593c4ee7f7eb5ef2769e3e6a3d22e76b160c46e
Janson Kang <temasek71@gmail.com>
2016-04-06 19:21:06 +0800

    Revert "Frameworks/base: Second Clock (1/2)"
    This reverts commit d77bf847decea2d9890f5c822c827f7fea95e791.
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
@@ -45,10 +45,7 @@ import com.android.systemui.cm.UserContentObserver;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.GregorianCalendar;
 import java.util.Locale;
-import java.util.Timer;
-import java.util.TimerTask;
 import java.util.TimeZone;
 
 import libcore.icu.LocaleData;
@@ -147,9 +144,6 @@ public class Clock extends TextView implements DemoMode {
             updateSettings();
         }
     }
-
-    private final Handler handler = new Handler();
-    TimerTask second;
 
     public Clock(Context context) {
         this(context, null);
@@ -312,11 +306,6 @@ public class Clock extends TextView implements DemoMode {
 
         String result = sdf.format(mCalendar.getTime());
 
-        if (Settings.System.getInt(mContext.getContentResolver(), Settings.System.CLOCK_USE_SECOND, 0) == 1) {
-            String temp = result;
-            result = String.format("%s:%02d", temp, new GregorianCalendar().get(Calendar.SECOND));
-        }
-
         if (mClockDateDisplay != CLOCK_DATE_DISPLAY_GONE) {
             Date now = new Date();
 
@@ -377,36 +366,17 @@ public class Clock extends TextView implements DemoMode {
 
     protected void updateSettings() {
         ContentResolver resolver = mContext.getContentResolver();
-
         mClockFormatString = "";
-	mClockcolor = Settings.System.getIntForUser(mContext.getContentResolver(),
+
+        mClockcolor = Settings.System.getIntForUser(mContext.getContentResolver(),
                 Settings.System.STATUSBAR_CLOCKCOLOR_SWITCH, 0,
                 UserHandle.USER_CURRENT) == 1;
-
         mClockDateDisplay = Settings.System.getIntForUser(resolver,
                 Settings.System.STATUS_BAR_DATE, CLOCK_DATE_DISPLAY_GONE,
                 UserHandle.USER_CURRENT);
         mClockDateStyle = Settings.System.getIntForUser(resolver,
                 Settings.System.STATUS_BAR_DATE_STYLE, CLOCK_DATE_STYLE_REGULAR,
                 UserHandle.USER_CURRENT);
-
-        second = new TimerTask()
-        {
-            @Override
-            public void run()
-             {
-                Runnable updater = new Runnable()
-                  {
-                   public void run()
-                   {
-                       updateClock();
-                   }
-                  };
-                handler.post(updater);
-             }
-        };
-        Timer timer = new Timer();
-        timer.schedule(second, 0, 1001);
 
         updateClock();
     }
