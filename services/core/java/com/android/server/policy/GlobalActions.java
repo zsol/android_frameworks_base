abc3b1efc3fd6e35befee019572b69fdd7ba1646
Tom Marshall <tdm@cyngn.com>
2016-06-28 09:43:10 -0700

    fw: GlobalActions: Always dismiss any existing dialog
    The check for "mUiContext == null" is always false after the initial
    dialog is created, causing the dismiss logic to never trigger.  All
    code uses getUiContext() now, so the check is not necessary.  Remove
    it and re-align the create/dismiss logic with AOSP.
    
    Jira: BACON-4069
    
    Change-Id: Ibba7b38a5fdc8410824db6d0a6f726f4a2822c9d
    
    Conflicts:
    	services/core/java/com/android/server/policy/GlobalActions.java

--- services/core/java/com/android/server/policy/GlobalActions.java
+++ services/core/java/com/android/server/policy/GlobalActions.java
@@ -252,14 +252,12 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
     public void showDialog(boolean keyguardShowing, boolean isDeviceProvisioned) {
         mKeyguardShowing = keyguardShowing;
         mDeviceProvisioned = isDeviceProvisioned;
-        if (mDialog != null && mUiContext == null) {
+        if (mDialog != null) {
             mDialog.dismiss();
             mDialog = null;
-            mDialog = createDialog();
             // Show delayed, so that the dismiss of the previous dialog completes
             mHandler.sendEmptyMessage(MESSAGE_SHOW);
         } else {
-            mDialog = createDialog();
             handleShow();
         }
     }
@@ -278,6 +276,7 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
 
     private void handleShow() {
         awakenIfNecessary();
+        mDialog = createDialog();
 	checkSettings();
         prepareDialog();
 
